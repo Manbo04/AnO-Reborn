b718b262cf3913483425efd3146e0c8679a554e3
Mantas Bonda <dede@Mantass-MacBook-Air.local>
2025-12-14 17:06:08 +0100

    attack_scripts: fix flake8 issues in Air_attack and Ground_attack (line lengths, unused vars, typos)

--- attack_scripts/Air_attack.py
+++ attack_scripts/Air_attack.py
@@ -1,13 +1,19 @@
 import random
 
 
-# TODO: get nation names from DB, get amount of fighters and bombers from DB,add infra damage,make aircraft consume resources
-# TODO: figure out how calculate bomber and fighter losses seperatly
+# TODO: get nation names from DB, get amount of fighters and bombers
+# from DB, add infra damage, make aircraft consume resources
+# TODO: figure out how calculate bomber and fighter losses separately
 
 
 # print statements
 def ground_attack(
-    nat1_name, nat2_name, nat1_fighters, nat2_fighters, Nat1Bomb, Nat2Bomb
+    nat1_name,
+    nat2_name,
+    nat1_fighters,
+    nat2_fighters,
+    Nat1Bomb,
+    Nat2Bomb,
 ):
     nat1_roll_wins = 0
     nat2_roll_wins = 0
@@ -15,14 +21,15 @@ def ground_attack(
     Nat1 = nat1_fighters
     Nat2 = nat2_fighters
 
-    for roll in range(0, 3):
-        nat1_roll = random.randint(1, Nat1 + 5 * Nat1Bomb)
-        nat2_roll = random.randint(1, Nat2 + 5 * Nat2Bomb)
+    for _roll in range(0, 3):
+        nat1_roll = random.randint(1, max(1, Nat1 + 5 * Nat1Bomb))
+        nat2_roll = random.randint(1, max(1, Nat2 + 5 * Nat2Bomb))
         print(
-            f"This is random output of aircraft going to dogfight for Nat1: {nat1_roll} out of {Nat1} Fighters {Nat1Bomb} Bombers.."
-        )
-        print(
-            f"This is random output of aircraft going to dogfight for Nat2: {nat2_roll} out of {Nat2} fighters and {Nat2Bomb} Bombers.."
+            (
+                "Dogfight rolls: Nat1=%s (fighters=%s, bombers=%s), "
+                "Nat2=%s (fighters=%s, bombers=%s)"
+            )
+            % (nat1_roll, Nat1, Nat1Bomb, nat2_roll, Nat2, Nat2Bomb)
         )
         # difference between the two rolls
         difference = abs(nat1_roll - nat2_roll)
@@ -32,13 +39,13 @@ def ground_attack(
             # subtract difference from the nations maximum if they rolled lower
             Nat2 -= difference
             # calulates bomber losses
-            Bombloss = random.randint(1, Nat1Bomb // 5) * 5
+            Bombloss = random.randint(1, max(1, Nat1Bomb // 5)) * 5
             Nat1Bomb = Nat1Bomb - Bombloss / 5
-            displayloss1 = Bombloss1 / 5
-            Bombloss2 = random.randint(1, Nat2Bomb // 5) * 5
+            displayloss1 = Bombloss / 5
+            Bombloss2 = random.randint(1, max(1, Nat2Bomb // 5)) * 5
             Nat2Bomb = Nat2Bomb - Bombloss2 / 5
             displayloss2 = Bombloss2 / 5
-            print(f"Nation 2 lost {displayloss2} Bombers")
+            print("Nation 2 lost %s Bombers" % displayloss2)
             # gives a 6% casualty rate for the nation that rolled larger
             six_percent_loss = int(Nat2 * 0.06)
             Nat1 -= six_percent_loss
@@ -53,17 +60,17 @@ def ground_attack(
             # subtract difference from the nations maximum if they rolled lower
             Nat1 -= difference
             # the twelve percent stored in new variable so it can be printed
-            Bombloss1 = random.randint(1, Nat1Bomb // 5) * 5
-            Nat1Bomb = Nat1Bomb - Bombloss / 5
+            Bombloss1 = random.randint(1, max(1, Nat1Bomb // 5)) * 5
+            Nat1Bomb = Nat1Bomb - Bombloss1 / 5
             displayloss1 = Bombloss1 / 5
-            Bombloss2 = random.randint(1, Nat2Bomb // 5) * 5
+            Bombloss2 = random.randint(1, max(1, Nat2Bomb // 5)) * 5
             Nat2Bomb = Nat2Bomb - Bombloss2 / 5
             displayloss2 = Bombloss2 / 5
             print(f"Nation 2 lost {displayloss2} Bombers")
             six_percent_loss = int(Nat1 * 0.06)
             Nat2 -= six_percent_loss
 
-            print(f"Nation 1 lost {displayloss1} Bombers")
+            print("Nation 1 lost %s Bombers" % displayloss1)
             # endsbattle if all are destroyed
             if Nat1 and Nat1Bomb <= 0:
                 Nat1 = 0
@@ -71,13 +78,12 @@ def ground_attack(
                 print("nation 2 won the battle")
                 break
 
-        print(f"This is the output after the battle: {difference}")
-        print(f"This is the 6 percent value from the battle: {six_percent_loss}")
+        print("Battle difference: %s" % difference)
+        print("6%% casualty value: %s" % six_percent_loss)
 
     if nat1_roll_wins > nat2_roll_wins:
-        return f"{nat1_name}won the battle"
-    else:
-        return f"{nat2_name} won the battle"
+        return f"{nat1_name} won the battle"
+    return f"{nat2_name} won the battle"
 
 
 ground_attack("Danzig", "Konigsburg", 630, 380, 4, 10)
