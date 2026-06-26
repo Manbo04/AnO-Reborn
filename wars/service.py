fc1492443362f3aadf4ddb3a549a27e25cdb4c2d
Odysseus <odysseus@agent>
2026-06-27 01:26:50 +0300

    fix: replace war_id and attacker_id in wars/service.py

--- wars/service.py
+++ wars/service.py
@@ -24,7 +24,7 @@ def update_supply(war_id):
         db.execute(
             (
                 "SELECT attacker_supplies,defender_supplies,last_visited "
-                "FROM wars WHERE war_id=%s"
+                "FROM wars WHERE id=%s"
             ),
             (war_id,),
         )
@@ -40,7 +40,7 @@ def update_supply(war_id):
         supply_by_hours = hours_count * 50  # 50 supply in every hour
         if supply_by_hours > 0:
             db.execute(
-                "SELECT attacker_id,defender_id FROM wars where war_id=(%s)", (war_id,)
+                "SELECT attacker,defender FROM wars where id=(%s)", (war_id,)
             )
             attacker_id, defender_id = db.fetchone()
             attacker_upgrades = Nation.get_upgrades("supplies", attacker_id)
@@ -51,26 +51,26 @@ def update_supply(war_id):
                 defender_supplies += i
             if (supply_by_hours + attacker_supplies) > MAX_SUPPLY:
                 db.execute(
-                    "UPDATE wars SET attacker_supplies=(%s) WHERE war_id=(%s)",
+                    "UPDATE wars SET attacker_supplies=(%s) WHERE id=(%s)",
                     (MAX_SUPPLY, war_id),
                 )
             else:
                 db.execute(
-                    "UPDATE wars SET attacker_supplies=(%s) WHERE war_id=(%s)",
+                    "UPDATE wars SET attacker_supplies=(%s) WHERE id=(%s)",
                     (supply_by_hours + attacker_supplies, war_id),
                 )
             if (supply_by_hours + defender_supplies) > MAX_SUPPLY:
                 db.execute(
-                    "UPDATE wars SET defender_supplies=(%s) WHERE war_id=(%s)",
+                    "UPDATE wars SET defender_supplies=(%s) WHERE id=(%s)",
                     (MAX_SUPPLY, war_id),
                 )
             else:
                 db.execute(
-                    "UPDATE wars SET defender_supplies=(%s) WHERE war_id=(%s)",
+                    "UPDATE wars SET defender_supplies=(%s) WHERE id=(%s)",
                     (supply_by_hours + defender_supplies, war_id),
                 )
             db.execute(
-                ("UPDATE wars SET last_visited=(%s) " "WHERE war_id=(%s)"),
+                ("UPDATE wars SET last_visited=(%s) " "WHERE id=(%s)"),
                 (time.time(), war_id),
             )
 
